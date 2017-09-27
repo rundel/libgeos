626cec1283058fb57cc75d7e808161e6562566c7
Colin Rundel <rundel@gmail.com>
2017-09-27 12:34:21 -0400

    show progress and cleanup

--- tools/download_zip.R
+++ tools/download_zip.R
@@ -4,20 +4,17 @@ url  = args[1]
 dir  = args[2]
 
 file = basename(url)
-download.file(url, file, quiet = TRUE)
+download.file(url, file, quiet = FALSE)
 
 ext = tolower(tools::file_ext(file))
 
-if (ext == "zip") {
-  unzip(file, exdir = dir)
-} else if (ext == "bz2") {
-  system2("tar", paste("xj","-C",dir,"-f",file), FALSE)
-} else if (ext == "gz") {
-  system2("tar", paste("xz","-C",dir,"-f",file), FALSE)
-} else if (ext == "tar") {
-  system2("tar", paste("x","-C",dir,"-f",file), FALSE)
-} else {
-  stop("Unknown file type")
-}
+switch(
+  ext,
+  { stop("Unknown file type: ", ext) },
+  zip = { unzip(file, exdir = dir) },
+  bz2 = { system2("tar", paste("xj","-C",dir,"-f",file), FALSE) },
+  gz  = { system2("tar", paste("xz","-C",dir,"-f",file), FALSE) },
+  tar = { system2("tar", paste("x", "-C",dir,"-f",file), FALSE) }
+)
 
 unlink(file)
